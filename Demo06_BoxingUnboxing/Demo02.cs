ffc50b743122870678bb866878a3394ccf9dedfe
Manoj Kumar Sharma <manojexamai@gmail.com>
2024-05-14 12:54:56 +0530

    Added the Boxing and Unboxing Demo

---
+++ Demo06_BoxingUnboxing/Demo02.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo06_BoxingUnboxing
+{
+    public class Driver
+    {
+        public void DriveVehicle(object obj)
+        {
+            // Throws error if object is not a Car type
+            // Car car = (Car)obj;                 // unboxing (EXPLICIT) 
+
+            if (obj.GetType() == typeof(Car))
+            {
+                Car car = (Car)obj;                 // unboxing (EXPLICIT) 
+                car.Drive();
+            }
+            else if (obj is Scooter)        // IS operator (type-checker operator)
+            {
+                // AS Operator is the SAFE TYPE CAST Operator
+                // AS Operator returns NULL if it is not able to typecast successfully!
+                Scooter? scooter = obj as Scooter;  // unboxing (EXPLICIT) with AS Operator
+
+                //if(scooter is not null)
+                //{
+                //    scooter.Drive();
+                //}
+
+                scooter?.Drive();       // NULL-SAFE operator (?.)
+            }
+        }
+
+        public void DriveCar(Car obj)
+        {
+            obj.Drive();
+        }
+
+        public void DriveScooter(Scooter obj)
+        {
+            obj.Drive();
+        }
+    }
+
+    public class Car
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Car is being driven");
+        }
+    }
+
+    public class Scooter
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Scooter is being driven");
+        }
+    }
+}
---
+++ Demo06_BoxingUnboxing/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Demo06_BoxingUnboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Demo01();
+
+            Demo02();
+        }
+
+        static void Demo01()
+        {
+            int i = 10;
+            object o = i;          // Boxing - IMPLICIT CONVERSION
+            Console.WriteLine("i = {0}, i.GetType() = {1}", i, i.GetType());
+            Console.WriteLine("o = {0}, o.GetType() = {1}", o, o.GetType());
+
+            int j = (int)o;        // Unboxing - EXPLICIT CONVERSION
+
+            i++;
+            // o++;
+            o = (int)o + 1;
+        }
+
+        static void Demo02()
+        {
+            Driver objDriver = new Driver();
+            Car objCar = new Car();
+            Scooter objScooter = new Scooter();
+
+            Console.WriteLine("-- Explicit Method invocation demo");
+            objDriver.DriveCar(objCar);
+            objDriver.DriveScooter(objScooter);
+
+            Console.WriteLine("-- Boxing and Unboxing demo");
+            objDriver.DriveVehicle(objCar);     // boxing IMPLICITLY
+            object objS = objScooter;           // boxing explicitly by assigning to variable.
+            objDriver.DriveVehicle(objS);
+        }
+
+    }
+}
